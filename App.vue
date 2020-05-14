e80fd2d03e27010f764aadbc80aad76e80330f95
wuqiuping <771500739@qq.com>
2020-05-14 15:35:38 +0800

    提交代码

--- App.vue
+++ App.vue
@@ -1,43 +1,49 @@
 <script>
-export default {
-	onLaunch: function() {
-		console.log('App Launch');
-	},
-	onShow: function() {
-		//console.log('App Show');
-		uni.hideTabBar();
-	},
-	onHide: function() {
-		console.log('App Hide');
-	}
-};
+	export default {
+		onLaunch: function() {
+			console.log('App Launch');
+		},
+		onShow: function() {
+			//console.log('App Show');
+			uni.hideTabBar();
+		},
+		onHide: function() {
+			console.log('App Hide');
+		}
+	};
 </script>
 
 <style>
-<<<<<<< HEAD
-  page { background-color:#F8F8F8;}
-  .commonweb{width:659.42rpx;margin:0 auto;}
-  *{box-sizing: border-box;margin:0;padding:0;}
-</style>
-=======
 	/*每个页面公共css */
 	@import "/static/font/iconfont.css";
-  page { background-color:#F8F8F8;font-size:21.73rpx;color: #4e5a65;}
-  .commonweb{width:659.42rpx;margin:0 auto;}
-  *{box-sizing: border-box;margin:0;padding:0;}
-  .tui-icon {
-  		width: 40rpx;
-  		height: 36.23rpx;
-  		background: #F23A3B;
-  		color: #fff;
-  		line-height: 36.23rpx;
-  		text-align: center;
-  		font-size: 21.73rpx;
-  		margin-right: 5px;
-		vertical-align:2px;
-		
-  }
-  	
-</style>
 
->>>>>>> 8a8f9163e0b7ae2612b8def3b211d16b37c95d31
+	page {
+		background-color: #F8F8F8;
+		font-size: 21.73rpx;
+		color: #4e5a65;
+	}
+
+	.commonweb {
+		width: 659.42rpx;
+		margin: 0 auto;
+	}
+
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	.tui-icon {
+		width: 40rpx;
+		height: 36.23rpx;
+		background: #F23A3B;
+		color: #fff;
+		line-height: 36.23rpx;
+		text-align: center;
+		font-size: 21.73rpx;
+		margin-right: 5px;
+		vertical-align: 2px;
+
+	}
+</style>
--- main.js
+++ main.js
@@ -2,13 +2,9 @@ import Vue from 'vue'
 import App from './App'
 
 import http from './utils/https.js'
-<<<<<<< HEAD
 import store from './store'
 
-=======
->>>>>>> 5cfba9c7ac1d09ea07a88a9571c497a7ae686b9e
 import pageFooter from './components/pageFooter'
-import goodsPrice from './components/goodsPrice'
 import status from './components/status.vue'
 
 Vue.config.productionTip = false
@@ -20,7 +16,6 @@ Vue.prototype.$backgroundAudioData = {
 	playTime: 0,
 	formatedPlayTime: '00:00:00'
 }
-
 App.mpType = 'app'
 //全局注册组件
 Vue.component('page-footer',pageFooter)
