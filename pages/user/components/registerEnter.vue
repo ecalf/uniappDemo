8d096e4064cf24515669b21326b02bfadad26644
wuqiuping <771500739@qq.com>
2020-05-18 15:01:23 +0800

    新建注册

---
+++ pages/user/components/registerEnter.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<view class="register-wrap">
+			<view class="register-item">
+				<image src="@/static/images/personal.png" mode=""></image>
+				<text>个人注册</text>
+			</view>
+			<view class="register-item">
+				<image src="@/static/images/personal.png" mode=""></image>
+				<text>个人注册</text>
+			</view>
+			<view class="register-item">
+				<image src="@/static/images/personal.png" mode=""></image>
+				<text>个人注册</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
